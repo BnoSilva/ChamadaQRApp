bfc5f5f1a558020927b421008f49a2ba144b690f
Bruno Alencar da Silva <31566970+BnoSilva@users.noreply.github.com>
2017-10-28 09:01:27 -0200

    Subindo o codigo

---
+++ app/src/main/java/br/com/fabricaapp/chamadaqr/MainActivity.java
@@ -0,0 +1,56 @@
+package br.com.fabricaapp.chamadaqr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.widget.*;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener {
+
+    EditText txt_Login, txt_Senha;
+    Button botao_Login, botao_Registrar;
+    AlertDialog alertaLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt_Login = (EditText) findViewById(R.id.txtUsuario);
+        txt_Senha = (EditText) findViewById(R.id.txtSenha);
+
+        botao_Login = (Button) findViewById(R.id.btnLogin);
+        botao_Login.setOnClickListener(this);
+
+        botao_Registrar = (Button) findViewById(R.id.btnRegistrar);
+        botao_Registrar.setOnClickListener(this);
+    }
+
+        public void onClick (View view){
+            switch (view.getId()) {
+                case R.id.btnLogin:
+                    String usuario = txt_Login.getText().toString();
+                    String senha = txt_Senha.getText().toString();
+                    if (usuario.equals("admin") && senha.equals("1234")) {
+                    Intent TelaPrincipal = new Intent(MainActivity.this, br.com.fabricaapp.chamadaqr.TelaPrincipal.class);
+                    startActivity(TelaPrincipal);
+                    } else {
+                    AlertDialog.Builder msgErro = new AlertDialog.Builder(this);
+                    msgErro.setTitle("Erro");
+                    msgErro.setMessage("Usuário ou Senha incorretos\nTente Novamente");
+                    alertaLogin = msgErro.create();
+                    alertaLogin.show();
+                    }
+                break;
+                case R.id.btnRegistrar:
+                    Intent TelaCadastro = new Intent(MainActivity.this, br.com.fabricaapp.chamadaqr.TelaCadastro.class);
+                    startActivity(TelaCadastro);
+                    break;
+            }
+        }
+
+    }

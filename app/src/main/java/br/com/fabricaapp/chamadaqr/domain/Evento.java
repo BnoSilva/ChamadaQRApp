7dcf1e16e9b8d3d65db309fa05ea9ed966e193b0
Bruno Alencar da Silva <31566970+BnoSilva@users.noreply.github.com>
2017-11-18 07:58:31 -0200

    Atualizando a lista de eventos

--- app/src/main/java/br/com/fabricaapp/chamadaqr/domain/Evento.java
+++ app/src/main/java/br/com/fabricaapp/chamadaqr/domain/Evento.java
@@ -2,17 +2,17 @@ package br.com.fabricaapp.chamadaqr.domain;
 
 
 public class Evento {
-    private final String nome;
-    private final String local;
-    private final String data;
-    private final String detalhes;
+    public String nome;
+    public String local;
+    public String data;
+    public String detalhes;
 
-    public Evento(String nome, String local, String data, String detalhes) {
+    /*public Evento(String nome, String local, String data, String detalhes) {
         this.nome = nome;
         this.local = local;
         this.data = data;
         this.detalhes = detalhes;
-    }
+    }*/
 
     public String getNome() {
         return nome;

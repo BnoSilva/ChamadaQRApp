cf225e83acce83d760401dd64cfad972c088dd52
Bruno Alencar da Silva <31566970+BnoSilva@users.noreply.github.com>
2018-03-24 10:49:42 -0300

    Novo App

--- app/src/main/java/br/com/fabricaapp/chamadaqr/activity/TelaPrincipalActivity.java
+++ app/src/main/java/br/com/fabricaapp/chamadaqr/activity/TelaPrincipalActivity.java
@@ -1,11 +1,15 @@
 package br.com.fabricaapp.chamadaqr.activity;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +19,8 @@ import br.com.fabricaapp.chamadaqr.R;
 import br.com.fabricaapp.chamadaqr.domain.Evento;
 import br.com.fabricaapp.chamadaqr.adapter.EventoAdapter;
 
+import static java.security.AccessController.getContext;
+
 
 public class TelaPrincipalActivity extends AppCompatActivity implements ClickRecyclerView {
 
@@ -22,7 +28,7 @@ public class TelaPrincipalActivity extends AppCompatActivity implements ClickRec
     private RecyclerView.LayoutManager nLayoutManager;
     EventoAdapter adapter;
     private List<Evento> listaEvento = new ArrayList<>();
-    private FloatingActionButton floatingActionButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +36,6 @@ public class TelaPrincipalActivity extends AppCompatActivity implements ClickRec
         setContentView(R.layout.activity_tela_principal);
 
         setRecyclerView();
-        setButtons();
-        listenerButtons();
     }
 
         public void setRecyclerView(){
@@ -42,35 +46,15 @@ public class TelaPrincipalActivity extends AppCompatActivity implements ClickRec
         adapter = new EventoAdapter(listaEvento, this, this);
         nRecyclerView.setAdapter(adapter);
 
-            for(int i = 0; i < 3; i++){
-                Evento evento = new Evento("Teste", "Teste", "Teste", "Teste");
+            for(int i = 0; i < 5; i++){
+                Evento evento = new Evento();
+                evento.nome = "Projeto: " + i;
                 listaEvento.add(evento);
             }
-    }
-
-
-    public void setButtons(){
-
-        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab_fabteste);
-
     }
 
     @Override
     public void onCustomClick(Object object) {
 
     }
-
-    public void listenerButtons(){
-        floatingActionButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-
-                Evento evento = new Evento("Teste", "Teste", "Teste", "Teste");
-
-                listaEvento.add(evento);
-                adapter.notifyDataSetChanged();
-            }
-        });
-    }
-
 }
--- app/src/main/java/br/com/fabricaapp/chamadaqr/domain/Evento.java
+++ app/src/main/java/br/com/fabricaapp/chamadaqr/domain/Evento.java
@@ -2,31 +2,26 @@ package br.com.fabricaapp.chamadaqr.domain;
 
 
 public class Evento {
-    private final String nome;
-    private final String local;
-    private final String data;
-    private final String detalhes;
+    private int idEvento;
+    public String nome;
 
-    public Evento(String nome, String local, String data, String detalhes) {
+
+    /*public Evento(String nome, String local, String data, String detalhes) {
         this.nome = nome;
         this.local = local;
         this.data = data;
         this.detalhes = detalhes;
-    }
+    }*/
 
     public String getNome() {
         return nome;
     }
 
-    public String getLocal() {
-        return local;
+    public int getIdEvento() {
+        return idEvento;
     }
 
-    public String getData() {
-        return data;
-    }
-
-    public String getDetalhes() {
-        return detalhes;
+    public void setIdEvento(int idEvento) {
+        this.idEvento = idEvento;
     }
 }
